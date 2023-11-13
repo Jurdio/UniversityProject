c79ffc22e8ae4f68cff0d0abbd8d8ad3126ee85b
Dmytro <mr.vacul@gmail.com>
2023-11-13 15:01:17 +0200

    add Main Class

---
+++ src/main/java/com/example/university/project/Main.java
@@ -0,0 +1,9 @@
+package com.example.university.project;
+
+import com.example.university.project.scenes.StartApp;
+
+public class Main {
+    public static void main(String[] args) {
+        StartApp.main(args);
+    }
+}
