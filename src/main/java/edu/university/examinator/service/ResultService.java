a4fdfc7f8578bfc26ddbd785e81f28a0d65f8485
Dmytro <dmytro.kupets@gmail.com>
2024-01-05 16:11:12 +0200

    feat: add ResultService

---
+++ src/main/java/edu/university/examinator/service/ResultService.java
@@ -0,0 +1,42 @@
+package edu.university.examinator.service;
+
+import edu.university.examinator.domain.Question;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Text;
+
+public class ResultService {
+    private final Text correctCountText;
+    private final Text incorrectCountText;
+    private int correctCount;
+    private int incorrectCount;
+    public ResultService(Text correctCountText, Text incorrectCountText){
+        this.correctCountText = correctCountText;
+        this.incorrectCountText = incorrectCountText;
+    }
+    private void updateCorrectCount(){
+        correctCount++;
+        correctCountText.setText(String.valueOf(correctCount));
+    }
+    private void updateIncorrectCount(){
+        incorrectCount++;
+        incorrectCountText.setText(String.valueOf(incorrectCount));
+    }
+    public void validateAnswer(ToggleGroup toggleGroup, Question question){
+        RadioButton selectedRadioButton = (RadioButton) toggleGroup.getSelectedToggle();
+
+        if(selectedRadioButton != null){
+            String userAnswer = selectedRadioButton.getText();
+
+            if (question.isCorrectAnswer(userAnswer)){
+                updateCorrectCount();
+            } else {
+                updateIncorrectCount();
+            }
+
+        } else {
+            updateIncorrectCount();
+        }
+    }
+
+}
